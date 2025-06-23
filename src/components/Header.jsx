a1986e1ea56f4d0306315568fa802fe7e1185123
Lynda BOUCHEBAH <bcb.lynda@gmail.com>
2025-06-23 16:41:27 +0200

    add MouseEvent function

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,15 @@
 import logo from "../assets/img/happycow-logo.svg";
 import { FaMagnifyingGlass } from "react-icons/fa6";
+import { useState } from "react";
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const handleMouseEnter = () => {
+    setIsOpen(true);
+  };
+  const handleMouseLeave = () => {
+    setIsOpen(false);
+  };
   return (
     <section className="header">
       <div className="logo">
@@ -10,7 +18,18 @@ const Header = () => {
       <div className="navigation">
         <div className="navigationLeft">
           <ul>
-            <ul className="menuExp">Explore</ul>
+            {handleMouseEnter === true ? (
+              <li>Salut</li>
+            ) : (
+              <li
+                onMouseEnter={handleMouseEnter}
+                onMouseLeave={handleMouseLeave}
+                className="menuExp"
+              >
+                Explore
+              </li>
+            )}
+
             <li>Forum</li>
             <li>Blog</li>
             <li>The App</li>
@@ -19,9 +38,9 @@ const Header = () => {
           </ul>
         </div>
         <div className="navigationRight">
-        {/* <button><i>{FaMagnifyingGlass}</i></button> */}
-        <button className="addList">Add Listing</button>
-        <button className="login">Login/Join</button>
+          <FaMagnifyingGlass className="buttonSearch" />
+          <button className="addList">Add Listing</button>
+          <button className="login">Login/Join</button>
         </div>
       </div>
     </section>
